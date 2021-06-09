8af8dbebc2c9e611cd92c6e6e7386d3a855a05a1
Liv <lopez.olivia17@outlook.com>
2021-06-08 20:02:54 -0500

    adjusted api.js and apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,14 +14,20 @@ db.Workout.find({})
   });
 });
 
-// Add Exercise | Update a workout 
+// Add Exercise
 router.put("/workouts/:id", (req, res) => {
   console.log(req.params.id)
-  db.Workout.updateOne(
-    {
-      _id: mongojs.ObjectId(req.params.id)
+  db.Workout.findByIdAndUpdate( req.params.id, 
+    { $push: 
+      // name: req.body.name,
+      // duration: req.body.duration,
+      // weight: req.body.weight, 
+      // reps: req.body.reps, 
+      // sets: req.body.sets,
+    { exercise: req.body }
     },
     (error, data) => {
+      console.log(data)
       if(error) {
         res.send(error);
       } else {
@@ -30,6 +36,31 @@ router.put("/workouts/:id", (req, res) => {
     }
   );
 });
+
+// Create Workout 
+router.post("/workouts", (req, res) => {
+  db.Workout.create(req.body)
+  .then(dbWorkout => {
+    console.log(dbWorkout)
+    res.json(dbWorkout)
+  })
+  .catch(err => {
+    console.error(err)
+    res.send(err);
+  });
+});
+
+// router.post("/workouts", (req, res) => {
+//   db.Workout.create(req.body)
+//   .then(dbWorkout => {
+//     console.log(dbWorkout)
+//     res.json(dbWorkout)
+//   })
+//   .catch(err => {
+//     console.error(err)
+//     res.send(err);
+//   });
+// });
 
 
 module.exports = router;
